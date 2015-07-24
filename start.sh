b5a01ea15e3d47c9f98e79f8c49133a2423a9aec
Thomas Geselle <raitomm@gmail.com>
2015-07-24 23:44:26 +0200

    Edit

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cd /home/mcmyadmin/ && \
     wget http://mcmyadmin.com/Downloads/MCMA2_glibc26_2.zip && \
 		unzip MCMA2_glibc26_2.zip && rm MCMA2_glibc26_2.zip
 
-/home/mcmyadmin/MCMA2_Linux_x86_64 -nonotice -updateonly -setpass "$PASSWORD" +java.memory $JAVA_MEMORY
+/home/mcmyadmin/MCMA2_Linux_x86_64 -setpass "$PASSWORD" +java.memory $JAVA_MEMORY
